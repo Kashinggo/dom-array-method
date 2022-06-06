e6eb02f93b20c991eee7434fa394d7f0d080daeb
kscode <kashinggo@gmail.com>
2022-06-06 12:20:55 +0800

    calculate wealth - reduce()

--- script.js
+++ script.js
@@ -52,6 +52,18 @@ function showMillionaires() {
   data = data.filter((user) => user.money > 1000000);
   updateDOM();
 }
+
+// Calculate total wealth
+function calculateWealth() {
+  const total = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    total
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 // Update DOM·
 function updateDOM(providedData = data) {
   main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
@@ -77,3 +89,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
